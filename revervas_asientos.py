08eb8a13e231d5db6b820dd452a79a609a5050f5
Roman Hdz Cz <146388340+RomanatMx@users.noreply.github.com>
2023-10-11 17:20:44 -0600

    Add files via upload
    Este es un ejercicio realizado a partir de una prueba técnica de trabajo dictada por el canal TodoCode en Youtube, me animé a realizar el reto por que creí que era un gran desafío para probar las capacidades que tengo ahora que llevo un mes tras haber empezado a programar.

---
+++ revervas_asientos.py
@@ -0,0 +1,73 @@
+'''
+Prueba técnica resuelta, consiste en crear un sistema de asientos para un anfiteatro. El teatro cuenta con 10 filas y
+10 columnas de asiento, este sistema debe de permitir al cliente poder elegir de entre los asientos libres el que desee 
+ocupar y a su vez reservar el asiento. 
+'''
+import os 
+
+#1er Paso. Creamos la matriz 10*10 la cual representa los asientos en el anfiteatro...
+asientos_anfiteatro = [
+                        ['L']*10,
+                        ['L']*10,
+                        ['L']*10,
+                        ['L']*10,
+                        ['L']*10,
+                        ['L']*10,
+                        ['L']*10,
+                        ['L']*10,
+                        ['L']*10,
+                        ['L']*10]
+print(asientos_anfiteatro[0][0])
+#2do Paso. Creamos el menú que nos permitirá mostrar el estado de los asientos, reservar asiento o salir del programa...
+RESERVAR = 1
+MOSTRAR = 2
+SALIR = 3
+def mostrar_menú():
+    os.system('cls')
+    print('                                                 Bienvenido al Anfiteatro')
+    print(f'''
+        {RESERVAR} Reservar asiento
+        {MOSTRAR} Mostrar asientos disponibles
+        {SALIR} Salir
+    ''')
+#definimos la función para la impresión del estado actual de asientos disponibles
+def MostrarAsientosDisponibles():
+    print(asientos_anfiteatro)
+
+#definimos la función para solicitar al cliente el asiento por fila y columna que desea ocupar
+#si el asiento se encuentra libre podrá ocuparlo y este se reservará con 'X' como señal de asiento ocupado
+#si el asiento se encuentra ocupado, enviará devuelta al cliente a la selección de la fila y columna 
+def ReservarAsientos():
+    print('                                                             Reserva tu asiento!')
+    fila = int(input('Fila, del 0 al 9: '))
+    columna = int(input('Columna, del 0 al 9: '))
+    if asientos_anfiteatro[fila][columna] == 'L':
+        print('El asiento está disponible, disfrute su película')
+        asientos_anfiteatro[fila][columna] = 'X'
+    elif asientos_anfiteatro[fila][columna] == 'X':
+        print('Lo sentimos el haciento está reservado')
+        ReservarAsientos()
+#definimos la función main o principal que es la que nos imprimirá el menu y nos permitirá elegir las opciones disponibles
+def main():
+    continuar = True
+    while continuar: 
+        os.system('cls')
+        mostrar_menú()
+        opc = int(input("Elige una opción: "))
+        os.system('cls')
+        if opc == RESERVAR:
+            ReservarAsientos()
+        elif opc == MOSTRAR:
+            MostrarAsientosDisponibles()
+        elif opc == SALIR:
+            continuar = False
+            print ('Te esperemos pronto!')
+        else:
+            print('Opción no válida.')
+        input('Presiona enter para continuar')
+
+#llamamos la la funcion main como la principal para la ejecución en la terminal
+if __name__ == '__main__':
+    main()
+        
+        
